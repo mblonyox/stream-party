e6a7f050ea4308360c9f73469020a7b8f4d148f6
Sukirno <mblonyox@gmail.com>
2022-03-13 18:01:42 +0700

    fix: add Player component

---
+++ src/components/Player.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import Plyr from 'plyr';
+
+
+const player = ref<HTMLDivElement>()
+
+onMounted(() => {
+  const plyr = new Plyr(player.value!);
+  console.log(plyr)
+})
+
+</script>
+
+<template>
+  <div class="flex-1 rounded-xl border bg-base-100 flex flex-col justify-center">
+    <video src="" ref="player"></video>
+  </div>
+</template>
+
+<style>
+@import url("plyr/dist/plyr.css");
+</style>
--- src/components/Room.vue
+++ src/components/Room.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import Chat from "./Chat.vue"
+import Player from "./Player.vue";
 </script>
 
 <template>
   <div class="flex gap-4 h-full">
-    <div class="card border flex-1"></div>
+    <Player />
     <Chat />
   </div>
 </template>
